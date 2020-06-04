99f7be76ef787f4f8f9cfa3a5e52b3466be396c4
LqZww <951598077@qq.com>
2020-06-04 09:17:32 +0800

    v8-网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,44 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    // get请求
+    // wx.request({
+    //   url: 'https://autumnfish.cn/related/playlist',
+    //   data: {
+    //     id: 33894312
+    //   },
+    //   success: res => {
+    //     console.log(res);
+    //   }
+    // })
 
-  },
+    // post请求
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "POST",
+    //   data: {
+    //     name: "zww",
+    //     age: 18
+    //   },
+    //   success: res => {
+    //     console.log(res);
+    //   }
+    // })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 封装
+    request({
+      url: "https://autumnfish.cn/related/playlist",
+      data: {
+        id: 33894312
+      }
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   }
 })
